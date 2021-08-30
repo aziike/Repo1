9dff3309a4ac9a10eae79db96bb1d43a58c9e3e5
azke <89734190+aziike@users.noreply.github.com>
2021-08-30 14:33:39 +0000

    modified comments

--- main.py
+++ main.py
@@ -23,15 +23,15 @@ else:
  # Set number of guesses to zero
  # print "Ok user Im guessing a number"
  # While number of guesses is less than 5 do below command
- #   Ask user to input guess number
- #   add one to number of guessess
+ #   ask user to input guess number
+ #   add one to the number of guessess
  #   if user's guess number is less than random number
  #       print "your guess is too low"
  #   if user's guess number is more than random number
  #       print "your guess is too high"
- #   if user's guess number is equall to random number
- #       break from while loop
- # if user's guess number equall the random number 
+ #   if user's guess number is equal to random number
+ #       stop while loop
+ # if user's guess number equal the random number 
  #    print " you guessed the number in number of guesses times tries"
  # otherwise print" you did not guess the number and number was: "
  #End program
